a831639409f9cfad0595d8359b2b2b56208412ce
Ahmad Ali <Ahmad>
2024-01-19 20:50:01 +0300

    added prisma

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Invoices() {
+    return <>
+        <p>Invoices</p>
+    </>
+}
--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
-      className={clsx(
+     className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
           'bg-gray-100 text-gray-500': status === 'pending',
